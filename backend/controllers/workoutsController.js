af4e6480e8c95bb522f53244a82d1e9eb267044d
ThevinRanasinghe <thevinmanvindu@gmail.com>
2026-05-28 21:14:23 +0530

    Updated workoutController.js with deleteWorkout and updateWorkout functions

--- backend/controllers/workoutsController.js
+++ backend/controllers/workoutsController.js
@@ -1,13 +1,31 @@
 const Workout = require('../models/workoutModel')
+const mongoose = require('mongoose')
+
 
 //get all workouts
-const getWorkout = async (req,res) => {
-    const workout = await Workout.find({}).sort({createdAt:1})
+const getWorkouts = async (req,res) => {
+    const workout = await Workout.find({}).sort({createdAt: -1})
     res.status(200).json(workout)
 }
 
 
 //get a single workout
+const getWorkout = async (req,res) => {
+    const {id} = req.params
+
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        res.status(404).json({error: 'No such workout'})
+    }
+
+    const workout = await Workout.findById(id)
+
+    if(!workout){
+        return res.status(404).json({error: 'Workout Not Found'})
+    }
+
+    res.status(200).json(workout)
+
+}
 
 
 //create a new workout
@@ -25,11 +43,47 @@ const createWorkout = async (req,res) =>{
 
 
 //delete a new workout
+const deleteWorkout = async(req,res) =>{
+    const {id} = req.params
+
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        res.status(404).json({error: 'No such Workout'})
+    }
+
+    const workout = await Workout.findOneAndDelete({_id: id})
+
+    if(!workout){
+        res.status(404).json({error: 'No Such Workout'})
+    }
+
+    res.status(200).json(workout)
+
+}
 
 
 //update a new workout
+const updateWorkout = async (req,res) => {
+    const {id} = req.params
+
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        res.status(404).json({error: 'No such Workout'})
+    }
+
+    const workout = await Workout.findOneAndUpdate({_id:id},{...req.body})
+
+    if(!workout){
+        res.status(404).json({error: 'No Such Workout'})
+    }
+
+    res.status(200).json(workout)
+}
 
 
 module.exports = {
-    createWorkout
+    getWorkouts,
+    getWorkout,
+    createWorkout,
+    deleteWorkout,
+    updateWorkout
+    
 }
